4705fca7c132787ddc272d0b526f45979f457d7d
neuhausler <neuhausler@gmail.com>
2020-01-07 15:43:55 -0800

    added upgrade option

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install streamlit
-pip install spacy
+pip install --upgrade streamlit
+pip install --upgrade spacy
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_md
